5120f84bf50e5a63587f0d9e4f0632b3b725d966
jtrinidad1 <jtrinidadj@gmail.com>
2012-11-12 23:37:08 -0500

    More Turtle exercises...

---
+++ SWFK/ex3.py
@@ -0,0 +1,30 @@
+#This will be a normal rectangle, followed by crazy triangles.
+import turtle
+t = turtle.Pen()
+
+t.forward(100)
+t.left(90)
+t.forward(140)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(140)
+
+
+t.reset()
+
+t.right(90)
+
+t.left(45)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(135)
+t.forward(280)
+
+
+
